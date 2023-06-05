8a25bd9975f8e7b42da9d1a12bdc6ef75952b322
Suziane Brandao <suzianebrandao90@alu.ufc.br>
2023-06-05 09:31:33 -0300

    instalação mysql

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config({path:'variaveis.env'});
-const express = require('express');
+const express = require ('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
