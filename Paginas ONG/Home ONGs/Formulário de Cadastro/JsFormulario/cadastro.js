4981a1946ae58d2e006060cf6d6d743b535e33bc
Suziane Brandao <suzianebrandao90@alu.ufc.br>
2023-06-06 21:32:13 -0300

    modificar alguns pacotes

--- Paginas ONG/Home ONGs/Formulário de Cadastro/JsFormulario/cadastro.js
+++ Paginas ONG/Home ONGs/Formulário de Cadastro/JsFormulario/cadastro.js
@@ -1,40 +1,13 @@
-class Validator {
-    constructor(){
-        this.validations=[
-            'data-min-length',
+var validator = require("email-validator");
+validator.validate("test@email.com");
 
-        ]
-    }
+const express = require('express');
 
-// iniciar a validação de todos os campos//
+const  server = express();
 
-    validate(form) {
+server.use(express.json());
 
-    let inputs = form.getElementsByTagName('input');    // pegar todos inputs
-    let inputsArray = [...inputs];     // transformar HTMLCollection em array
-    inputsArray.forEach(function(input){
-        
-        for(let i = 0; this.validations.length > i; i++){
-            if(input.getAttribute(this.validations[i] != null)){
-                console.log('achou a validaçao')
-            }
-        }
-    }, this)
+const ongs = [CNPJ, nome, email, endereco, senha]
 
-    
 
-    }
-
-}
-
-let form = document.getElementsByClassName("input-group");
-
-let submet = document.getElementsByClassName("cadastro-button");
-let validator = new Validator();
-
-// disparar a validação//
-
-submet.addEventListener('click', function(e){
-e.preventDefalt();
-validator.validate(form);
-})
+server.listen(3000);
