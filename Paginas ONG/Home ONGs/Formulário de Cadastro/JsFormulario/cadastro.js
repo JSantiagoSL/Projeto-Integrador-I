d9c6fda56956a468a65b05f16a906f91d7f6f87b
Suziane Brandao <suzianebrandao90@alu.ufc.br>
2023-06-12 23:53:04 -0300

    classe ONG

--- Paginas ONG/Home ONGs/Formulário de Cadastro/JsFormulario/cadastro.js
+++ Paginas ONG/Home ONGs/Formulário de Cadastro/JsFormulario/cadastro.js
@@ -1,13 +1,27 @@
-var validator = require("email-validator");
-validator.validate("test@email.com");
-
-const express = require('express');
-
-const  server = express();
-
-server.use(express.json());
-
-const ongs = [CNPJ, nome, email, endereco, senha]
-
-
-server.listen(3000);
+class Ong{
+    constructor(nome, CNPJ, endereco, senha, img){
+    this.nome = nome;
+    this.CNPJ = CNPJ;
+    this.endereco = endereco;
+    this.senha = senha;
+    this.img = img;
+    }
+exibirOng(){
+    const tableBody = document.querySelector("tabelaOng tbody");
+    const row = document.createElement("tr");
+    row.innerHTML = ` 
+    <td>${this.nome}</td>
+    <td>${this.CNPJ}</td>
+    <td>${this.endereco}</td>
+    <td>${this.senha}</td>
+    <td>${this.img}</td>
+    <td>
+      <button class="editarBtn">Editar</button>
+      <button class="excluirBtn">Excluir</button>
+    </td>
+    `;
+    tableBody.appendChild(row);
+    
+    
+}
+}
