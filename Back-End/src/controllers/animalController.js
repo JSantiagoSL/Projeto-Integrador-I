3515336f228f66882ff62cec1dba75294ffd7182
Suziane Brandao <suzianebrandao90@alu.ufc.br>
2023-07-03 19:29:36 -0300

    animalController e AnimalService conectados

--- Back-End/src/controllers/animalController.js
+++ Back-End/src/controllers/animalController.js
@@ -1,58 +1,123 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const userService = require('../services/animalService');
 
-const animalController = {
-  getAllAnimals: async (req, res) => {
+async function createAnimal(req, res) {
     try {
-      const animals = await prisma.animal.findMany();
-      res.json(animals);
+        const {id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong} = req.body
+        let animal = await animalService.findUserById(id_animal);
+
+        if (animal) {
+            return res.json({
+                success: false,
+                data: {},
+                message: "User with this email already exist",
+            });
+        }
+
+        animal = await animalService.createAnimal(id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong)
+
+        return res.json({
+            success: true,
+            data: user,
+            message: "User created successfully",
+        });
+
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+        return res.json({error})
     }
-  },
-
-  getAnimalById: async (req, res) => {
-    const { id } = req.params;
+}
+async function findAllAnimais(req, res) {
     try {
-      const animal = await prisma.animal.findUnique({
-        where: { id: Number(id) },
-      });
-      if (animal) {
-        res.json(animal);
-      } else {
-        res.status(404).json({ error: 'Animal not found' });
-      }
+        const animais = await animalService.findAllAnimais();
+        return res.json({
+            success: true,
+            data: animais,
+            message: "animais found successfully",
+        });
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  },
+        return res.json({error})
 
-  createAnimal: async (req, res) => {
-    const { name, species, age } = req.body;
+    }
+}
+
+async function findAnimal(req, res){
     try {
-      const animal = await prisma.animal.create({
-        data: { name, species, age },
-      });
-      res.json(animal);
+        const {id} = req.params;
+        const animal = await animalService.findAnimalById(id);
+
+        if (!animal){
+            return res.json({
+                    success: false,
+                    data: {},
+                    message: "Could not find this user",
+            });
+        }
+
+        return res.json({
+            success: true,
+            data: animal,
+            message: "animal found successfully",
+        });
+    }catch (error) {
+        return res.json({error})
+
+    }
+}
+
+async function updateAnimal(req, res){
+    try {
+        const {id} = req.params;
+        const {id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong} = req.body;
+
+        let animal = await animalService.updateAnimal(id);
+
+        if (!animal){
+            return res.json({
+                success: false,
+                data: {},
+                message: "Could not update this user",
+            });
+        }
+        animal = await animalService.updateAnimal(id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong);
+
+        return res.json({
+            success: true,
+            data: animal,
+            message: "User updated successfully",
+        });
+
+    }catch (error) {
+        return res.json({error})
+
+    }
+}
+async function deleteAnimal(req, res) {
+    try {
+        const { id } = req.params;
+
+        const animal = await animalService.findAnimalById(id);
+        if (!animal){
+            return res.json({
+                success: false,
+                data: {},
+                message: "Could not find this animal",
+            });
+        }
+
+        await animalService.deleteAnimalById(id);
+        return res.json({
+            success: true,
+            data: animal,
+            message: "User deleted successfully",
+        });
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+        return res.json({ error });
     }
-  },
+}
 
-  updateAnimal: async (req, res) => {
-    const { id } = req.params;
-    const { name, species, age } = req.body;
-    try {
-      const updatedAnimal = await prisma.animal.update({
-        where: { id: Number(id) },
-        data: { name, species, age },
-      });
-      res.json(updatedAnimal);
-    } catch (error){
-        res.status(500).json({ error: 'Internal server error' });
-    }
-  }
+module.exports = {
+    createAnimal,
+    findAllAnimais,
+    findAnimal,
+    updateAnimal,
+    deleteAnimal,
 };
-
-
-
---
+++ Back-End/src/services/animalService.js
@@ -0,0 +1,61 @@
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+
+async function createAnimal(id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong) {
+    const animal = await prisma.animal.create({
+        data: {
+            id_animal, 
+            especie, 
+            sexo,
+            porte, 
+            idade, 
+            pelagem, 
+            cor, 
+            deficiencia, 
+            vacinas, 
+            descricao, 
+            ong,
+            
+        },
+    });
+
+    return animal;
+}
+
+async function findAnimalById(id_animal) {
+    return prisma.animal.findUnique({
+        where: {id: Number(id_animal)}
+    });
+}
+
+
+async function findAllAnimais() {
+
+    const animais = await prisma.animal.findMany();
+    return animais;
+}
+
+async function updateAnimal(id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong) {
+
+    const animal = await prisma.animal.update({
+        where: { id: Number(id_animal) },
+        data: {id_animal, especie, sexo, porte, idade, pelagem, cor, deficiencia, vacinas, descricao, ong}
+    });
+
+    return animal;
+}
+
+async function deleteAnimalById(id_animal) {
+
+    return prisma.animal.delete({where: {id: Number(id_animal)}});
+}
+module.exports = {
+    createAnimal,
+    findAllAnimais,
+    deleteAnimalById,
+    updateAnimal,
+    findAnimalById,
+};
