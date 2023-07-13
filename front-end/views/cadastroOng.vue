bf6f58602c472b83929cfa1e6c10fb59e354a23d
Suziane Brandao <suzianebrandao90@alu.ufc.br>
2023-07-13 14:34:32 -0300

    modificacoes

---
+++ front-end/views/cadastroOng.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <Banner />
+      <div class="main-container">
+        <h1>Cadastre sua Ong</h1>
+        <ongForm />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import ongForm from '../components/ongForm'
+  
+  export default {
+    name: 'Home',
+    components: {
+      ongForm
+    }
+  }
+  </script>
