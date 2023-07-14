397d0dff1ea61a25bad3588aef2a4f62ef2817f1
Suziane Brandao <suzianebrandao90@alu.ufc.br>
2023-07-14 01:19:58 -0300

    front inicializado

---
+++ teste/src/views/GerenciarAnimais.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <div class="main-container">
+        <h1>Gerenciar animais:</h1>
+        <Dashboard />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Dashboard from '../components/Dashboard'  //quadros com informações dos animais
+  
+  export default {
+    name: 'Animais',
+    components: {
+      Dashboard
+    }
+  }
+  </script>
